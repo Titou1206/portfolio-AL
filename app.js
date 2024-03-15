40cf64ed728793c8dce0ebb97b19db1fe96d85a5
Titou1206 <laugierantoine@gmail.com>
2024-03-15 16:32:04 +0100

    v15/03/24pm
    mise en page header-hero-about ok
    début de mise en page portfolio et dimensionnement cart projet

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ colorMode.addEventListener("change",(e)=>{
         body.setAttribute("data-theme","dark")
     }
 })
+
+let switchAccess = document.getElementById("switch-access")
+let btnAccess = document.getElementById("button-access")
+switchAccess.addEventListener("change",(e)=>{
+    if(switchAccess.checked){
+        body.setAttribute("data-style","access")
+        btnAccess.classList.add("p-access-checked")
+    }else{
+        body.setAttribute("data-style","normal")
+        btnAccess.classList.remove("p-access-checked")
+    }
+})
