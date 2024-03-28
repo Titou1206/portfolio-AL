365c5ae6ce00c9c8657801a86860281064a815b3
Titou1206 <laugierantoine@gmail.com>
2024-03-28 13:10:39 +0100

    V-28/03/24-13/10
    responsive ok - slider a peu pres ok

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+// récupération de l'input du burger par son selecteur css
+let burger = document.querySelector(".burger")
+// surveillance du click sur le burger et lance la fonction affiche menu
+burger.addEventListener("click", (e) => {
+	afficheMenu()
+})
+
+function afficheMenu(){
+// role : affiche le menu et modifie le logo burger
+// parametre : rien
+// retour : rien
+	// récupération du menu par son selecteur css
+	let menu = document.querySelector(".menu")
+	// enleve ou ajoute la class menu-ouvert
+	menu.classList.toggle("menu-ouvert")
+	// récupération du hero, main et footer
+	let blurs = document.querySelectorAll(".menu-blur")
+	// enleve ou ajoute la class blur2
+	blurs.forEach(blur=>{
+		blur.classList.toggle("blur2")
+	})
+	// recupération des 3 trait du burger
+	let burgerTop = document.querySelector("span.top")
+	let burgerMiddle = document.querySelector("span.middle")
+	let burgerBottom = document.querySelector("span.bottom")
+	// incline les trait haut et bas avec les class associé et fais s'échapper le trait du milieu
+	burgerTop.classList.toggle("top-check")
+	burgerMiddle.classList.toggle("middle-check")
+	burgerBottom.classList.toggle("bottom-check")
+}
+
 // Dark mode
 // récupération du body
 let body = document.querySelector("body")
@@ -83,40 +114,26 @@ function initSwiper(){
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-    
-        // If we need pagination
-        pagination: {
-        el: '.swiper-pagination',
-        },
-    
-        // Navigation arrows
-        navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        },
         // Default parameters
-        width: 320,
+
         slidesPerView: 1,
         spaceBetween: 20,
         // Responsive breakpoints
         breakpoints: {
             // when window width is >= 320px
-            750: {
-                width: 660,
+            760: {
                 slidesPerView: 2,
-                spaceBetween: 20
+                spaceBetween: 30
             },
             // when window width is >= 480px
             1300: {
-                width: 1050,
                 slidesPerView: 3,
-                spaceBetween: 30
+                spaceBetween: 40
             },
             // when window width is >= 640px
-            1700: {
-                width: 1400,
+            1800: {
                 slidesPerView: 4,
-                spaceBetween: 40
+                spaceBetween: 50
             }
         }
     });
@@ -145,22 +162,22 @@ function createSlide(projets){
         swiper.innerHTML += `<div class="swiper-slide">
             <div class="card-inner">
                 <div class="card-front">
-                    <h4 class="text-center">${projet.titre}</h4>
+                    <h4 class="large-11 mrlauto text-center">${projet.titre}</h4>
                     <div class="projet-img mt32"><img src="assets/captures-projets/${projet.image}" alt="copie d'écran du projet mntn"></div>
-                    <p class="mt32">${projet.description}</p>
-                    <div class="flex justify-between mt32">
+                    <p class="large-11 mrlauto mt32">${projet.description}</p>
+                    <div class="large-11 mrlauto flex justify-between mt32">
                         <p>${projet.date}
                         <p>...</p>        
                     </div>
                 </div>
                 <div class="card-back">
-                    <h4>${projet.type}</h4>
-                    <div class="flex  gap16 mt32 align-center">
+                    <h4 class="large-11 mrlauto text-center">${projet.type}</h4>
+                    <div class="flex gap16 mt32 align-center justify-center">
                         ${pics}
                         ${libs}
                     </div>
-                    <p class="mt32 mb80">${projet.objectif}</p>
-                    <a target="_blank" href="${projet.lien}">${projet.lien}</a>
+                    <p class="large-11 mrlauto mt32 mb80">${projet.objectif}</p>
+                    <a class="large-11 mrlauto flex justify-center" target="_blank" href="${projet.lien}">${projet.lien}</a>
                 </div>
             </div>
         </div>`
