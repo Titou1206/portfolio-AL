831f9ce87f9bb1372dd3dec0eb8053bba759f62d
Titou1206 <laugierantoine@gmail.com>
2024-03-30 10:41:08 +0100

    V-30/03/24-10/40
    Menu burger fonctionnel

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ burger.addEventListener("click", (e) => {
 
 // récupération du menu par son selecteur css
 let menu = document.querySelector(".menu")
+let liens = menu.querySelectorAll(".nav-page")
+liens.forEach(lien=>{
+    lien.addEventListener("click",(e)=>{
+        afficheMenu()
+    })
+})
+
+
+// Ajout d'un écouteur d'événements pour fermer le menu en dehors de celui-ci
+document.addEventListener("click", (e) => {
+    // Vérifier si le clic n'a pas été fait dans le menu ou le burger
+    if (!menu.contains(e.target) && !burger.contains(e.target)) {
+        // Fermer le menu
+        removeMenu();
+    }
+});
+
+
 function afficheMenu(){
 // role : affiche le menu et modifie le logo burger
 // parametre : rien
@@ -30,6 +48,28 @@ function afficheMenu(){
 	burgerMiddle.classList.toggle("middle-check")
 	burgerBottom.classList.toggle("bottom-check")
 }
+
+function removeMenu(){
+    // role : affiche le menu et modifie le logo burger
+    // parametre : rien
+    // retour : rien
+        // enleve ou ajoute la class menu-ouvert
+        menu.classList.remove("menu-ouvert")
+        // récupération du hero, main et footer
+        let blurs = document.querySelectorAll(".menu-blur")
+        // enleve ou ajoute la class blur2
+        blurs.forEach(blur=>{
+            blur.classList.remove("blur2")
+        })
+        // recupération des 3 trait du burger
+        let burgerTop = document.querySelector("span.top")
+        let burgerMiddle = document.querySelector("span.middle")
+        let burgerBottom = document.querySelector("span.bottom")
+        // incline les trait haut et bas avec les class associé et fais s'échapper le trait du milieu
+        burgerTop.classList.remove("top-check")
+        burgerMiddle.classList.remove("middle-check")
+        burgerBottom.classList.remove("bottom-check")
+    }
 
 
 
@@ -269,3 +309,6 @@ window.addEventListener("mousemove", (e)=> {
     lumiere.style.left = e.clientX + "px"
     lumiere.style.top = e.clientY + "px"
 })
+
+
+
